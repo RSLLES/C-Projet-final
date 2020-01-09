db0679b4fc0077fe6903f68c5ad65e1d146f8c9a
Romain Seailles <romain.seailles@mines-paristech.fr>
2020-01-09 13:48:45 +0100

    Test avec tm

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
 #include "Priority_Status.hpp"
 #include <string>
+#include <iostream>
+#include <ctime>
 using namespace std;
 
 int main(){
-    Priority p;
-    cout << p.Get() << endl;
-    p.Set(1);
-    cout << p.Get() << endl;
-    p.Set("Low");
-    cout << p.Get() << endl;
-    p.Set(4);
-    cout << p.Get() << endl;
-};
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    cout << "Year" << 1900 + ltm->tm_year << endl;
+   cout << "Month: "<< 1 + ltm->tm_mon<< endl;
+   cout << "Day: "<<  ltm->tm_mday << endl;
+   cout << "Time: "<< 1 + ltm->tm_hour << ":";
+   cout << 1 + ltm->tm_min << ":";
+   cout << 1 + ltm->tm_sec << endl;
+
+   ltm->tm_mday = 5;
+   cout << "Year" << 1900 + ltm->tm_year << endl;
+   cout << "Month: "<< 1 + ltm->tm_mon<< endl;
+   cout << "Day: "<<  ltm->tm_mday << endl;
+   cout << "Time: "<< 1 + ltm->tm_hour << ":";
+   cout << 1 + ltm->tm_min << ":";
+   cout << 1 + ltm->tm_sec << endl;
+    return 0;
+}
