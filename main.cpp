7e408900e1977aea84c44c8ec1fa8169cee8d0d2
Romain Seailles <romain.seailles@mines-paristech.fr>
2020-01-10 17:03:49 +0100

    Test with Date

--- main.cpp
+++ main.cpp
@@ -1,25 +1,8 @@
-#include "Priority_Status.hpp"
-#include <string>
-#include <iostream>
-#include <ctime>
+#include "Date.hpp"
 using namespace std;
 
 int main(){
-    time_t now = time(0);
-    tm *ltm = localtime(&now);
-    cout << "Year" << 1900 + ltm->tm_year << endl;
-   cout << "Month: "<< 1 + ltm->tm_mon<< endl;
-   cout << "Day: "<<  ltm->tm_mday << endl;
-   cout << "Time: "<< 1 + ltm->tm_hour << ":";
-   cout << 1 + ltm->tm_min << ":";
-   cout << 1 + ltm->tm_sec << endl;
-
-   ltm->tm_mday = 5;
-   cout << "Year" << 1900 + ltm->tm_year << endl;
-   cout << "Month: "<< 1 + ltm->tm_mon<< endl;
-   cout << "Day: "<<  ltm->tm_mday << endl;
-   cout << "Time: "<< 1 + ltm->tm_hour << ":";
-   cout << 1 + ltm->tm_min << ":";
-   cout << 1 + ltm->tm_sec << endl;
+    Date today;
+    today.Print();
     return 0;
 }
