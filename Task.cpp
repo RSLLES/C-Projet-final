85234f70434dd5b7b68e379ed8ac3809cbdabfc8
Romain Seailles <romain.seailles@mines-paristech.fr>
2020-01-11 20:34:30 +0100

    Working StringToExport

--- Task.cpp
+++ Task.cpp
@@ -11,13 +11,10 @@ using namespace std;
 //Task
 //###########################
 
-Task::Task(int _id, string _title) : id(_id), title(_title), description(), pourcentage(0), priority(), status()
-{
-    //On intialise la date de départ
-    starting_date = new Date();
-}
+Task::Task(int _id, string _title) : id(_id), title(_title), description(), pourcentage(0), priority(), status(), starting_date(new Date()), ending_date(nullptr) {}
 
 vector<string> split(const string& s, char delimiter)
+//Fonction retournant un vector<string> a partir d'un string en spératant a chaque caractère delimiter
 {
    vector<string> tokens;
    string token;
@@ -29,13 +26,12 @@ vector<string> split(const string& s, char delimiter)
    return tokens;
 }
 
-Task::Task(string line) : id(-1), title("ERROR"), description(), pourcentage(0), priority(), status(){
-    starting_date = new Date();
+Task::Task(string line) : id(-1), title("ERROR"), description(), pourcentage(0), priority(), status(), starting_date(new Date()), ending_date(nullptr) {
     //Au cas ou le ficheir serait illisible, on créer une tache de type ERREUR
     //On créer la tache à partir de ce qui est noté dans la ligne.
     //On utilise pour cela la fonction split qui coupe au niveau des ; en renvoyant un vector<string>
     vector<string> elements(split(line, ';'));
-    if (elements.size() != 12){ //Si jamais il n'y a pas le bon nombre d'éléments
+    if (elements.size() != 12 || elements.size() != 9){ //Si jamais il n'y a pas le bon nombre d'éléments
         cout << "Impossible de construire la tache à partir de : " << line << endl;
     }
     else
@@ -49,7 +45,7 @@ Task::Task(string line) : id(-1), title("ERROR"), description(), pourcentage(0),
         starting_date->SetYear(stoi(elements[5]));
 
         //On ne créer la date de fin que si elle existe
-        if (elements[6] != ""){
+        if ( elements.size() == 12){
             ending_date = new Date(stoi(elements[6]),stoi(elements[7]),stoi(elements[8]));
         }
 
@@ -71,10 +67,15 @@ void Task::Print(){
 
 string Task::StringToExport(){
     //On utilise le point-virgule comme symbole séparateur
-    return(id + ";" + title  + ";" + description  + ";" + 
-    to_string(starting_date->GetDay()) + ";" + to_string(starting_date->GetMonth()) + ";" + to_string(starting_date->GetMonth()) +
-    to_string(ending_date->GetDay()) + ";" + to_string(ending_date->GetMonth()) + ";" + to_string(ending_date->GetMonth()) +
-    status.Get() + ";" + to_string(pourcentage)   + ";" + priority.Get());
+    string s;
+    s = to_string(id) + ";" + title  + ";" + description  + ";" + 
+    to_string(starting_date->GetDay()) + ";" + to_string(starting_date->GetMonth()) + ";" + to_string(starting_date->GetYear()) + ";";
+    if (ending_date != nullptr)
+    {
+        s+= to_string(ending_date->GetDay()) + ";" + to_string(ending_date->GetMonth()) + ";" + to_string(ending_date->GetYear()) + ";";
+    }
+    s+= status.Get() + ";" + to_string(pourcentage) + ";" + priority.Get();
+    return s;
 }
 
 
